ecb57b5a822942cfa24e4432b770e4c084cdc124
Ilija <ilija.beslin@gmail.com>
2024-04-17 22:34:32 +0200

    feat: started certificate handling

---
+++ src/main/java/rs/ac/uns/ftn/BookingBaboon/pki/dtos/IssuerDTO.java
@@ -0,0 +1,16 @@
+package rs.ac.uns.ftn.BookingBaboon.pki.dtos;
+
+import lombok.Data;
+
+@Data
+public class IssuerDTO {
+    private String commonName;
+    private String surname;
+    private String givenName;
+    private String organization;
+    private String organizationalUnit;
+    private String country;
+    private String email;
+    private String userId;
+
+}
